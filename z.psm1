4a0c7fc8fc6c351ad3bd370dc2ada7ee5cee4e38
Le Tuan Hai <letuanhai@live.com>
2023-03-22 14:33:30 +0700

    Reformat help comment

--- z.psm1
+++ z.psm1
@@ -1,45 +1,34 @@
-﻿$safehome = if ([String]::IsNullOrWhiteSpace($Env:HOME)) { $env:USERPROFILE } else { $Env:HOME } 
+$safehome = if ([String]::IsNullOrWhiteSpace($Env:HOME)) { $env:USERPROFILE } else { $Env:HOME } 
 $cdHistory = Join-Path -Path $safehome -ChildPath '\.cdHistory'
 
 <#
-
 .SYNOPSIS
-
-   Tracks your most used directories, based on 'frecency'. This is done by storing your CD command history and ranking it over time.
+Tracks your most used directories, based on 'frecency'. This is done by storing your CD command history and ranking it over time.
 
 .DESCRIPTION
-
-    After  a  short  learning  phase, z will take you to the most 'frecent'
-    directory that matches the regex given on the command line.
+After a short learning phase, z will take you to the most 'frecent' directory that matches the regex given on the command line.
 
 .PARAMETER JumpPath
-
 A un-escaped regular expression of the directory name to jump to. Character escaping will be done internally.
 
 .PARAMETER Option
-
 Frecency - Match by frecency (default)
 Rank - Match by rank only
 Time - Match by recent access only
 
 .PARAMETER OnlyCurrentDirectory
-
 Restrict matches to subdirectories of the current directory
 
 .PARAMETER Listfiles
-
 List only, don't navigate to the directory
 
-.PARAMETER $Remove
-
+.PARAMETER Remove
 Remove the current directory from the datafile
 
-.PARAMETER $Clean
-
+.PARAMETER Clean
 Clean up all history entries that cannot be resolved
 
 .NOTES
-
 Current PowerShell implementation is very crude and does not yet support all of the options of the original z bash script.
 Although tracking of frequently used directories is obtained through the continued use of the "cd" command, the Windows registry is also scanned for frequently accessed paths.
 
@@ -48,17 +37,12 @@ Although tracking of frequently used directories is obtained through the continu
    https://github.com/vincpa/z
 
 .EXAMPLE
-
 CD to the most frecent directory matching 'foo'
-
 z foo
 
 .EXAMPLE
-
 CD to the most recently accessed directory matching 'foo'
-
 z foo -o Time
-
 #>
 function z {
     param(
